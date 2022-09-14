28edc1e596c0017c52b3ae41b6fb921022b68196
Nate Seilaff <seilaffn@gmail.com>
2022-09-14 10:17:19 -0600

    Commit

--- app.js
+++ app.js
@@ -4,7 +4,34 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Nate";
+let lastName = "";
+var age = 0;
 // Exercise 2
+age = 27;
 
+lastName = "Seilaff";
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
+
+
+console.log(firstName, lastName, age, "\n", language, createdYear);
+console.log(price, salesPercentage, stock, selectedSize);
+console.log(title, author, pageCount, bookMark);
